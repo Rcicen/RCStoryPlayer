fc62a43891602bad219ce4c92fb61a2f92bbd9a7
Radun Çiçen <radunn92@gmail.com>
2020-07-25 22:01:11 +0300

    Created gesture recognizers for StoryPreviewCell

--- RCStoryPlayer/RCStoryPlayer/View/PreviewViews/StoryPreviewCell.swift
+++ RCStoryPlayer/RCStoryPlayer/View/PreviewViews/StoryPreviewCell.swift
@@ -15,8 +15,41 @@ class StoryPreviewCell: UICollectionViewCell,ReusableView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
         containerView.backgroundColor = .darkGray
     }
 
+    private lazy var tapGesture:UITapGestureRecognizer = {
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(didTapScreen(_:)))
+        recognizer.cancelsTouchesInView = false;
+        recognizer.numberOfTapsRequired = 1
+        recognizer.delegate = self
+        return recognizer
+    }()
+    
+    private lazy var longPressGesture: UILongPressGestureRecognizer = {
+        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress(_:)))
+        recognizer.minimumPressDuration = 0.2
+        recognizer.delegate = self
+        return recognizer
+    }()
+    
+    
+    //MARK: UIGestureRecognizer Actions
+    @objc func didTapScreen(_ sender: UITapGestureRecognizer) {
+        //TODO: Configure
+    }
+    
+    @objc func didLongPress(_ sender: UILongPressGestureRecognizer) {
+        //TODO: Configure
+    }
+}
+
+
+
+extension StoryPreviewCell:UIScrollViewDelegate {
+    //TODO: Configure
+}
+
+extension StoryPreviewCell:UIGestureRecognizerDelegate {
+    // TODO: Simultanious recognition
 }
