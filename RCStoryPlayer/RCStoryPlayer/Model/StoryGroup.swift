21a30f6d2006884701824354a3b18af3738047b2
Radun Çiçen <radunn92@gmail.com>
2020-07-26 12:50:23 +0300

    Empty model files generated

---
+++ RCStoryPlayer/RCStoryPlayer/Model/StoryGroup.swift
@@ -0,0 +1,13 @@
+//
+//  StoryGroup.swift
+//  RCStoryPlayer
+//
+//  Created by Radun Cicen on 26.07.2020.
+//  Copyright © 2020 raduncicen.com. All rights reserved.
+//
+
+import Foundation
+
+class StoryGroup {
+    
+}
---
+++ RCStoryPlayer/RCStoryPlayer/Model/StoryGroups.swift
@@ -0,0 +1,13 @@
+//
+//  StoryGroups.swift
+//  RCStoryPlayer
+//
+//  Created by Radun Cicen on 26.07.2020.
+//  Copyright © 2020 raduncicen.com. All rights reserved.
+//
+
+import Foundation
+
+class StoryGroups {
+    
+}
