6fde341f56c67130ad6657931935114ff98be1b0
Radun Çiçen <radunn92@gmail.com>
2020-07-28 00:24:54 +0300

    UIImageView imageWithUrl extension added photoView network requests added storyIndex change partially configured

---
+++ RCStoryPlayer/RCStoryPlayer/Extension/UIImageView.swift
@@ -0,0 +1,34 @@
+//
+//  UIImageView.swift
+//  RCStoryPlayer
+//
+//  Created by Radun Cicen on 28.07.2020.
+//  Copyright © 2020 raduncicen.com. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func ImageWithURL(_ string: String, completion:@escaping ((Bool)->()) ) {
+        self.image = nil
+        if let url = URL(string: string) {
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                if error != nil {
+                    print("Failed to load images from url: \(String(describing: error))")
+                    completion(false)
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let data = data {
+                        if let downloadedImage = UIImage(data: data) {
+                            self.image = downloadedImage
+                            completion(true)
+                        }
+                    }
+                }
+            }).resume()
+        } else {
+            completion(false)
+        }
+    }
+}
--- RCStoryPlayer/RCStoryPlayer/View/PreviewViews/StoryPreviewCell.swift
+++ RCStoryPlayer/RCStoryPlayer/View/PreviewViews/StoryPreviewCell.swift
@@ -8,43 +8,9 @@
 
 import UIKit
 
+let storyContentViewTagIdentifier = 300
+
 class StoryPreviewCell: UICollectionViewCell,ReusableView {
-
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var containerView: UIView!
-    
-    let storyHeaderView: PreviewHeaderView = {
-        let view = PreviewHeaderView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    var storyGroup:StoryGroup? {
-        didSet {
-            storyHeaderView.storyGroup = storyGroup
-        }
-    }
-    
-    var storyIndex:Int = 0
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        scrollView.delegate = self
-        scrollView.addGestureRecognizer(tapGesture)
-        scrollView.addGestureRecognizer(longPressGesture)
-        installStoryHeaderViewConstraints()
-    }
-
-    func installStoryHeaderViewConstraints() {
-        addSubview(storyHeaderView)
-        NSLayoutConstraint.activate([
-        storyHeaderView.topAnchor.constraint(equalTo: self.topAnchor),
-        storyHeaderView.leftAnchor.constraint(equalTo: self.leftAnchor),
-        storyHeaderView.rightAnchor.constraint(equalTo: self.rightAnchor),
-        storyHeaderView.heightAnchor.constraint(equalToConstant: 100)
-        ])
-    }
-
     
     private lazy var tapGesture:UITapGestureRecognizer = {
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(didTapScreen(_:)))
@@ -61,16 +27,123 @@ class StoryPreviewCell: UICollectionViewCell,ReusableView {
         return recognizer
     }()
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var containerView: UIView!
     
-    //MARK: UIGestureRecognizer Actions
+    let storyHeaderView: PreviewHeaderView = {
+        let view = PreviewHeaderView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    /// Returns the photoView at index
+    var currentPhotoView: UIImageView? {
+        if let imageView = containerView.subviews.filter({$0.tag == storyIndex + storyContentViewTagIdentifier}).first as? UIImageView {
+            return imageView
+        }
+        return nil
+    }
+    
+    var storyGroup:StoryGroup? {
+        didSet {
+            storyHeaderView.storyGroup = storyGroup
+        }
+    }
+    
+    var storyIndex:Int = 0
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        scrollView.delegate = self
+        scrollView.addGestureRecognizer(tapGesture)
+        scrollView.addGestureRecognizer(longPressGesture)
+        installStoryHeaderViewConstraints()
+    }
+    
+    func installStoryHeaderViewConstraints() {
+        addSubview(storyHeaderView)
+        NSLayoutConstraint.activate([
+            storyHeaderView.topAnchor.constraint(equalTo: self.topAnchor),
+            storyHeaderView.leftAnchor.constraint(equalTo: self.leftAnchor),
+            storyHeaderView.rightAnchor.constraint(equalTo: self.rightAnchor),
+            storyHeaderView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    /// Creates and constraints a new imageView to scrollView
+    func createPhotoView() -> UIImageView {
+        let photoImageView = UIImageView()
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        photoImageView.tag = storyIndex + storyContentViewTagIdentifier
+        containerView.addSubview(photoImageView)
+        
+        NSLayoutConstraint.activate([
+            photoImageView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            photoImageView.leadingAnchor.constraint(equalTo: storyIndex == 0 ? containerView.leadingAnchor : containerView.subviews[storyIndex - 1].trailingAnchor),
+            photoImageView.heightAnchor.constraint(equalTo: containerView.heightAnchor),
+            photoImageView.widthAnchor.constraint(equalTo: containerView.widthAnchor)
+        ])
+        return photoImageView
+    }
+    
+    func storyIndexChanged() {
+        if storyIndex < storyGroup?.storyCount ?? 0 {
+            if let story = storyGroup?.stories[storyIndex] {
+                if story.kind == .image {
+                    if let currentPhotoView = self.currentPhotoView {
+                        currentPhotoView.ImageWithURL(story.url) { (isSuccess) in
+                            if isSuccess {
+                                
+                            }else {
+                                
+                            }
+                        }
+                    }else {
+                        let newPhotoView = createPhotoView()
+                        newPhotoView.ImageWithURL(story.url) { (isSuccess) in
+                            if isSuccess {
+                                
+                            }else {
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func changeStoryIndex(to index:Int) {
+        guard let storyCount = storyGroup?.storyCount else { return }
+        if index >= 0 && index < storyCount {
+            let contentOffSet = CGPoint(x: containerView.frame.width * CGFloat(index), y: 0)
+            scrollView.setContentOffset(contentOffSet, animated: false)
+            storyGroup?.lastPlayedStoryIndex = index
+            storyIndex = index
+        } else {
+            //TODO: Move to the next StoryGroup ( CollectionView needs to move next index)
+        }
+    }
+    
+    
+    //MARK: - GESTURE RECOGNIZER ACTIONS
     @objc func didTapScreen(_ sender: UITapGestureRecognizer) {
         //TODO: Configure
+        guard let storyCount = storyGroup?.storyCount else {
+            return
+        }
+
         let touchLocation = sender.location(ofTouch: 0, in: scrollView)
         
-        if touchLocation.x < scrollView.contentOffset.x + (scrollView.frame.width/2) {
-            //TODO: Go to previous story or previous userStory (Goes to last displayed story index of that user)
-        } else {
+        if touchLocation.x > scrollView.contentOffset.x + (scrollView.frame.width/2) {
             //TODO: Go to next story or next userStory (Goes to last displayed story index of that user)
+            if storyIndex >= 0 && storyIndex <= storyCount {
+                changeStoryIndex(to: storyIndex + 1)
+            }
+        } else {
+            //TODO: Go to previous story or previous userStory (Goes to last displayed story index of that user)
+            
         }
         
     }
